65cacb42d78b8b0e3c5c0863906fe2312becfa28
duli <duli4868@gmail.com>
2025-05-18 17:53:11 +0800

    Initial Commit

---
+++ early-init.el
@@ -0,0 +1,18 @@
+;;; early-init.el --- amadeus-emacs -*- lexical-binding: t; -*-
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+(let ((dirs (list "straight" "emacs")))
+  (if (seq-every-p (lambda (dir)
+                     (file-symlink-p (concat user-emacs-directory dir)))
+                   dirs)
+      (dolist (dir dirs)
+        (set (intern (concat "amadeus-" dir "-directory")) (concat user-emacs-directory dir)))
+    (message ":: Run pre-setup.el first")
+    (kill-emacs 1)))
+
+(provide 'early-init)
+;;; early-init.el ends here
